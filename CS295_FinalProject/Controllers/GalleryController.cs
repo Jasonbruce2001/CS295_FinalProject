9dfa0e7542c21af5c7455bb0a37ecd293ef9f51b
Jasonbruce2001 <46138154+Jasonbruce2001@users.noreply.github.com>
2024-11-30 19:04:32 -0800

    Commit 11/30

--- CS295_FinalProject/Controllers/GalleryController.cs
+++ CS295_FinalProject/Controllers/GalleryController.cs
@@ -1,6 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CS295_FinalProject.Models;
+
 namespace CS295_FinalProject.Controllers;
 
-public class GalleryController
+public class GalleryController : Controller
 {
-    
+    private readonly ILogger<GalleryController> _logger;
+
+    public GalleryController(ILogger<GalleryController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Submissions()
+    {
+        return View();
+    }
+
+    public IActionResult Events()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
 }
--- CS295_FinalProject/Controllers/StoreController.cs
+++ CS295_FinalProject/Controllers/StoreController.cs
@@ -1,6 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CS295_FinalProject.Models;
+
 namespace CS295_FinalProject.Controllers;
 
-public class StoreController
+public class StoreController : Controller
 {
-    
+    private readonly ILogger<StoreController> _logger;
+
+    public StoreController(ILogger<StoreController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Reviews()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
 }
