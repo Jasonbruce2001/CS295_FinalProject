8454971aa47cb1dcce64dc166732b821ccddce6a
Jasonbruce2001 <46138154+Jasonbruce2001@users.noreply.github.com>
2024-12-08 16:34:47 -0800

    Pre Review Change

--- CS295_FinalProject/Data/SeedData.cs
+++ CS295_FinalProject/Data/SeedData.cs
@@ -6,6 +6,26 @@ public class SeedData
 {
     public static void Seed(ApplicationDbContext context)
     {
+        if (!context.Products.Any())
+        {
+            Product product1 = new Product { Name = "Adjustable Wooden Easel", Description = "Full pine adjustable easel with carrying case" +
+                                             "and folds down completely. Full height is 5 feet 6 inches", UnitPrice = 160, QuantityOnHand = 6 };
+            Product product2 = new Product { Name = "Artist Loft Brush Set", Description = "7 piece synthetic brush set perfect for beginners", 
+                                             UnitPrice = 25, QuantityOnHand = 20 };
+            Product product3 = new Product { Name = "General Mills Charcoal", Description = "2 piece compressed charcoal stick pack", UnitPrice = 5, QuantityOnHand = 50 };
+            Product product4 = new Product { Name = "Titanium White Acrylic Paint", Description = "100ml tube by Winsor and Newton", UnitPrice = 10, QuantityOnHand = 32 };
+            Product product5 = new Product { Name = "12x24 Cotton Canvas Panel", Description = "100% cotton stretched canvas, pre primed for acrylic paint", 
+                                             UnitPrice = 8, QuantityOnHand = 24 };
+            
+            context.Products.Add(product1);
+            context.Products.Add(product2);
+            context.Products.Add(product3);
+            context.Products.Add(product4);
+            context.Products.Add(product5);
+            
+            context.SaveChanges();
+        }
+        
         if (!context.Users.Any())  // this is to prevent adding duplicate data
         {
             // Create User objects
